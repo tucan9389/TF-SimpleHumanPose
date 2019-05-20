19835b1c0ecce3f2ed250afc8f9b9e9be06c9cd1
Doyoung Gwak <tucan.dev@gmail.com>
2019-05-20 13:57:53 +0900

    no message

--- main/train.py
+++ main/train.py
@@ -10,18 +10,18 @@ from tfflat.utils import mem_info
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu', type=str, dest='gpu_ids')
+    # parser.add_argument('--gpu', type=str, dest='gpu_ids')
     parser.add_argument('--continue', dest='continue_train', action='store_true')
     args = parser.parse_args()
 
-    if not args.gpu_ids:
-        args.gpu_ids = str(np.argmin(mem_info()))
-
-    if '-' in args.gpu_ids:
-        gpus = args.gpu_ids.split('-')
-        gpus[0] = 0 if not gpus[0].isdigit() else int(gpus[0])
-        gpus[1] = len(mem_info()) if not gpus[1].isdigit() else int(gpus[1]) + 1
-        args.gpu_ids = ','.join(map(lambda x: str(x), list(range(*gpus))))
+    # if not args.gpu_ids:
+    #     args.gpu_ids = str(np.argmin(mem_info()))
+    #
+    # if '-' in args.gpu_ids:
+    #     gpus = args.gpu_ids.split('-')
+    #     gpus[0] = 0 if not gpus[0].isdigit() else int(gpus[0])
+    #     gpus[1] = len(mem_info()) if not gpus[1].isdigit() else int(gpus[1]) + 1
+    #     args.gpu_ids = ','.join(map(lambda x: str(x), list(range(*gpus))))
 
     return args
 args = parse_args()
